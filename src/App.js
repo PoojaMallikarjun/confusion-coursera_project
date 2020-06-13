ed969b3efa6bba1b97cc417a4c100e4561016553
Pooja Mallikarjun <poojadotm702@gmail.com>
2020-06-13 22:11:27 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { BrowserRouter } from "react-router-dom";
 import Main from "./components/MainComponent";
 
 import "./App.css";
@@ -7,9 +7,11 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
