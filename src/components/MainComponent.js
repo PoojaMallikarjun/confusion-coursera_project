7849e598e71240d519cb4333dcba4ad1bcb90ef9
Pooja Mallikarjun <poojadotm702@gmail.com>
2020-06-16 00:42:21 +0530

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
 import { LEADERS } from "../shared/leaders";
@@ -53,6 +54,10 @@ class Main extends Component {
       );
     };
 
+    const Leaders = ({ match }) => {
+      return <About leaders={this.state.leaders} />;
+    };
+
     return (
       <div>
         <Header />
@@ -75,8 +80,9 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
-          <Route path="/menu/:dishId" component={DishWithId}></Route>
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={Leaders} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
